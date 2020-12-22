578dfd07a997059006427828842bc551052d055d
Anton Knyazev <anton.knyazev@omega-r.com>
2020-12-22 18:18:19 +0300

    Add empty project

---
+++ app/src/main/java/com/omega_r/click/app/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.omega_r.click.app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
