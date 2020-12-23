0ab500b9c452f5523152e62b86dc10822f4e892f
Anton Knyazev <anton.knyazev@omega-r.com>
2020-12-23 12:52:33 +0300

    Add sample

--- app/src/main/java/com/omega_r/click/app/MainActivity.kt
+++ app/src/main/java/com/omega_r/click/app/MainActivity.kt
@@ -2,10 +2,21 @@ package com.omega_r.click.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.omega_r.click.ClickManager
+import com.omega_r.click.OmegaClickable
+import com.omega_r.click.OmegaViewFindable
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OmegaClickable, OmegaViewFindable {
+
+    override val clickManager: ClickManager = ClickManager(this)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setOnClickListener(R.id.button_click_me) {
+            Toast.makeText(this, R.string.weel_done, Toast.LENGTH_LONG).show()
+        }
     }
+
 }
